91279ad5dbfa4a4202b56a6b448518a9a7a3be6a
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2024-01-02 21:48:54 -0700

    Initial release

---
+++ src/stack.cpp
@@ -0,0 +1,55 @@
+#include "stack.h"
+
+using namespace stack;
+
+// node methods
+Node::Node(float value){
+    value_ = value;
+}
+
+// updates the next node and inherits the next node's previous node
+void Node::setNext(Node* next){
+    next_ = next;
+    if (next)
+        next_->setPrev(this);
+}
+
+// updates the previous node and inherits the previous node's next node
+void Node::setPrev(Node* prev){
+    prev_ = prev;
+}
+
+// updates the node's value
+void Node::setValue(float value){
+    value_ = value;
+}
+
+// append a new node of a given value on the stack
+void Stack::push(float value){
+    size_++;
+    Node* tmp = tail_;
+    tail_ = new Node(value);
+    tail_->setPrev(head_);
+    if (tmp)
+        tmp->setNext(tail_);
+}
+
+// pop the tail from the top of the stack and delete it
+float Stack::pop(){
+    size_--;
+    Node* tmp = tail_;
+    float val = tail_->value();
+    if(tail_->prev()){
+        tail_ = tail_->prev();
+        tail_->setNext(nullptr);
+    }
+    delete tmp;
+    return val;
+
+}
+
+// returns if the node is empty
+bool Stack::isEmpty(){
+    return size_ == 0;
+}
+
---
+++ src/stack.h
@@ -0,0 +1,41 @@
+#ifndef STACK_H
+#define STACK_H 
+
+namespace stack{
+    class Node{
+        private:
+            float value_;
+            Node *next_;
+            Node *prev_;
+        public:
+            Node() : value_(0), next_(nullptr), prev_(nullptr) {}
+            Node(float value);
+            // setter methods
+            void setValue(float value);
+            void setNext(Node *next);
+            void setPrev(Node *prev);
+            // getter methods
+            float value() {return value_;}
+            Node *next(){return next_;}
+            Node *prev(){return prev_;}
+
+    };
+
+    class Stack{
+        private:
+            int size_;
+            Node *head_;
+            Node *tail_;
+        public:
+            Stack() : size_(0), head_(nullptr), tail_(nullptr) {};
+            void push(float val);
+            float pop();
+            bool isEmpty();
+            // getter methods
+            float top(){return tail_->value();}
+            int size(){return size_;}
+
+    };
+}
+
+#endif

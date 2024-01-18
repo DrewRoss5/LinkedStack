03a0eabbd5446b300b154e252da66a361da065ce
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2024-01-17 20:02:21 -0700

    Remove namespace and add templates

--- src/stack.h
+++ src/stack.h
@@ -1,45 +1,45 @@
 #ifndef STACK_H
 #define STACK_H 
 
-namespace stack{
-    class Node{
-        private:
-            float value_;
-            Node *next_;
-            Node *prev_;
-        public:
-            Node() : value_(0), next_(nullptr), prev_(nullptr) {}
-            Node(float value);
-            // setter methods
-            void setValue(float value);
-            void setNext(Node *next);
-            void setPrev(Node *prev);
-            // getter methods
-            float value() {return value_;}
-            Node *next(){return next_;}
-            Node *prev(){return prev_;}
+template<class T>
+class Node{
+    private:
+        T value_;
+        Node<T> *next_;
+        Node<T> *prev_;
+    public:
+        Node<T> (T value);
+        // setter methods
+        void setValue(T value);
+        void setNext(Node<T> *next);
+        void setPrev(Node<T> *prev);
+        // getter methods
+        T value() {return value_;}
+        Node<T> *next(){return next_;}
+        Node<T> *prev(){return prev_;}
+};
 
-    };
+template<class T>
+class Stack{
+    private:
+        int size_;
+        Node<T> *head_;
+        Node<T> *tail_;
+    public:
+        Stack() : size_(0), head_(nullptr), tail_(nullptr) {};
+        Stack copy();
+        void push(T val);
+        T pop();
+        void clear();
+        bool isEmpty();
+        // getter methods
+        Node<T> head(){return *head_;}
+        T top(){return tail_->value();}
+        int size(){return size_;}
+        // addition opperators
+        Stack<T> operator+(T val);
+        void operator+=(T val);
 
-    class Stack{
-        private:
-            int size_;
-            Node *head_;
-            Node *tail_;
-        public:
-            Stack() : size_(0), head_(nullptr), tail_(nullptr) {};
-            Stack copy();
-            void push(float val);
-            float pop();
-            bool isEmpty();
-            // getter methods
-            float top(){return tail_->value();}
-            int size(){return size_;}
-            // addition opperators
-            Stack operator+(float val);
-            void operator+=(float val);
-
-    };
-}
+};
 
 #endif
